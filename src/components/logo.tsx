b8609bf8c3f9eab60fbc7ae320029aeb490ece55
Rudi <kacarudi@gmail.com>
2025-01-15 20:58:57 +0100

    refactor

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import { appConfig } from "@/config/app";
 export function Logo() {
     return (
         <>
-            <img src="/public/free-lufthansa-282468.webp" alt="Logo" className="h-6 w-6" />
+            <img src="/free-lufthansa-282468.webp" alt="Logo" className="h-6 w-6" />
             <span className="font-bold">{appConfig.name}</span>
         </>
     )
